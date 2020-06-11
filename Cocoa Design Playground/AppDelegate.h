a15319907aa7972272d345832aa38ce2937fde66
David Wright <davidtwright89@gmail.com>
2020-06-10 21:41:09 -0400

    Initial Commit

---
+++ Cocoa Design Playground/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cocoa Design Playground
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Cocoa Design Playground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cocoa Design Playground
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
