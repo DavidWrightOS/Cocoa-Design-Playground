ef4f2617924e41629177a7e9d1b4a7861adcce36
David Wright <davidtwright89@gmail.com>
2020-06-10 21:42:05 -0400

    Added a PersonController class

---
+++ Cocoa Design Playground/PersonController.m
@@ -0,0 +1,13 @@
+//
+//  PersonController.m
+//  Cocoa Design Playground
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "PersonController.h"
+
+@implementation PersonController
+
+@end
