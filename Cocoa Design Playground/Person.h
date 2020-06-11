7614d8ebed24830f7088e7fa00a10151c4b088fa
David Wright <davidtwright89@gmail.com>
2020-06-10 22:48:26 -0400

    Explored an example of enums

--- Cocoa Design Playground/Person.h
+++ Cocoa Design Playground/Person.h
@@ -8,9 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSUInteger, PersonHairColor) {
+    PersonHairColorBrown = 0,
+    PersonHairColorBlonde = 1,
+    PersonHairColorOrange = 2,
+    PersonHairColorBlue = 3,
+    PersonHairColorTeal = 4,
+};
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface Person : NSObject
+
+@property (nonatomic, copy) NSString *fullName;
+@property (nonatomic) NSDate *birthDate;
+@property (nonatomic) PersonHairColor hairColor; // Enumerations are a C type, so they are not initialized as a pointer *
+
+- (instancetype)initWithName:(NSString *)aName birthDate:(NSDate *)aBirthDate hairColor:(PersonHairColor)hairColor;
 
 @end
 
