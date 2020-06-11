78cc95abafe19b7926ef74dfb0a03d46002abbfa
David Wright <davidtwright89@gmail.com>
2020-06-10 22:36:46 -0400

    Added a Person model object

---
+++ Cocoa Design Playground/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  Cocoa Design Playground
+//
+//  Created by David Wright on 6/10/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
