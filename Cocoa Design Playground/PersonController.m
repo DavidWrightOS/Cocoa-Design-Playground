09ef0a517e90fbacedac4327e94c828a3b60415f
David Wright <davidtwright89@gmail.com>
2020-06-10 22:00:56 -0400

    Updated the singleton to use the most modern approach

--- Cocoa Design Playground/PersonController.m
+++ Cocoa Design Playground/PersonController.m
@@ -14,11 +14,18 @@
 {
     static PersonController *personController = nil; // global, but only accessible in this method
     
-    if (!personController) {
-        personController = [[PersonController alloc] init];
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        personController = [[PersonController alloc] initSharedInstance];
         
-        // configure your person controller here
-    }
+        // configure your person controller
+    });
+    
+//    if (!personController) {
+//        personController = [[PersonController alloc] init];
+//
+//        // configure your person controller here
+//    }
     
     return personController;
 }
